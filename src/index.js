054d4a524723f38b240b244cd9160dd9862d153f
wangtunan <why583440138@gmail.com>
2024-04-08 21:34:58 +0800

    DynamicProgramming: 动态规划(0-1)背包问题代码实现

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-import { grid, minPathSumDPComp } from './dynamicProgramming/minPathSum.js';
+import { knapsackDPComp } from './dynamicProgramming/knapsack.js';
 
-console.log(minPathSumDPComp(grid));
+const wgt = [10, 20, 30, 40, 50];
+const val = [50, 120, 150, 210, 240];
+const cap = 50;
+
+const member = Array.from({ length: 6 }, () => new Array(51).fill(-1));
+
+console.log(knapsackDPComp(wgt, val, cap));
